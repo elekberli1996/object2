340424fb970883d57532e870c185e1e33e6252eb
emin elekberli <elekberli1996@gmail.com>
2024-04-02 23:04:33 +0400

    commit

---
+++ app.js
@@ -0,0 +1,57 @@
+// let products=[
+//     {
+//         id:1,
+//         name:"iphone 15",
+//         price:1500
+
+//     },
+//     {
+//         id:2,
+//         name:"iphone 15 pro",
+//         price:2500
+
+//     },
+//     {
+//         id:3,
+//         name:"samsung s23 ulta",
+//         price:200
+
+//     }
+//     ,
+//     {
+//         id:4,
+//         name:"samsung a50",
+//         price:800
+
+//     },
+//     {
+//         id:5,
+//         name:"samsung a70",
+//         price:850
+
+//     }
+// ];
+
+
+// //task 1
+
+// let new_array=products.sort(function(a,b){return a.price-b.price} );
+// console.log(new_array)
+
+// task 2 RANDOMM EDEDLER
+
+// let array =[];
+
+// a=Math.floor(Math.random()*10+1);
+// for (let i=0;i<5;i++){
+//     a=Math.floor(Math.random()*10+1);
+//      array.push(a);
+// }
+// let cem=0;
+// array.map(a=>cem+=a)
+
+// console.log(array,` arrayin cemi= ${cem}`)
+
+
+// for donguye daha cox hakim olmaqimizi sagliyir ve aray uzerimde emeliyat yerine yetror
+// map yeni bir array yaradir ve icersi return edir
